0e440848e45dc29b8a3eae7d489051e6bdc751bd
alexbejann <474404@student.saxion.nl>
2020-09-28 22:06:15 +0300

    Added status codes package and updated code with it

--- src/api/isAdmin.js
+++ src/api/isAdmin.js
@@ -1,1 +1,19 @@
 const { StatusCodes } = require('http-status-code');
+
+//Check if user is admin
+module.exports = function isAdmin (req,res,next)
+{
+    if (req.user.roles.indexOf('admin') === -1)
+    {
+        res
+            .status(StatusCodes.UNAUTHORIZED)
+            .send({
+                "error":"You are not admin!"
+            });
+    }
+    else
+    {
+        console.log('You can manage you bids');
+        next();
+    }
+}
